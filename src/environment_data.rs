c4882d36aab12727bd0a1a0443d8437f78ee8646
atigun <uqsfh@student.kit.edu>
2025-12-10 19:30:40 +0100

    seperated price and electricity prognoses from eachother

--- src/environment_data.rs
+++ src/environment_data.rs
@@ -1,12 +1,12 @@
 use crate::{
     action::{constant::ConstantAction, variable::VariableAction},
-    prognoses::Prognoses,
+    prognoses::{ElectricityPrognoses, PricePrognoses},
 };
 
 pub struct EnvironmentData {
-    electricity_price: Prognoses,
-    generated_electricity: Prognoses,
-    beyond_control_consumption: Prognoses,
+    electricity_price: PricePrognoses,
+    generated_electricity: ElectricityPrognoses,
+    beyond_control_consumption: ElectricityPrognoses,
 
     constant_actions: Vec<ConstantAction>,
     variable_actions: Vec<VariableAction>,
--- src/prognoses.rs
+++ src/prognoses.rs
@@ -1,3 +1,8 @@
-pub struct Prognoses {
+const TIME_INTERVAL: usize = 24 * 60;
+
+pub struct PricePrognoses {
+    data: [i32; 24 * 60],
+}
+pub struct ElectricityPrognoses {
     data: [i32; 24 * 60],
 }

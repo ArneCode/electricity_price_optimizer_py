465dd0f0b9f080fff11a2d54f8895519900c1d43
atigun <uqsfh@student.kit.edu>
2025-12-11 16:19:24 +0100

    fixed buged

--- src/optimizer/mod.rs
+++ src/optimizer/mod.rs
@@ -14,22 +14,39 @@ struct optimizer {
 
 impl optimizer {
     pub fn new(state: &State) -> Self {
-        let variable_maker = VariableMaker::new(state.get_context());
+        let variable_map = VariableMaker::new(state.get_context());
 
-        let SYSTEM_FLOW = variable_maker::SYSTEM_FLOW;
+        let WIRE = variable_maker::WIRE;
 
-        let mut mf = MinCostFlow::new(variable_maker.get_variable_count() as usize, variable_maker::SOURCE as usize, variable_maker::SINK as usize);
+        let mut mf = MinCostFlow::new(variable_map.get_variable_count() as usize, variable_maker::SOURCE as usize, variable_maker::SINK as usize);
     
-        mf.add_edge(variable_maker::SOURCE as usize, variable_maker::NETWORK as usize, 0, 0);
-        mf.add_edge(variable_maker::SOURCE as usize, variable_maker::GENERATOR as usize, 0, 0);
+        // Go from Source to Fork with Cost = 0, Capacity = total flow to complete tasks
+        let total_flow = 64; // to change
+        mf.add_edge(variable_maker::SOURCE as usize, variable_maker::FORK_FROM_SOURCE as usize, total_flow, 0);
 
+        // Seperate from fork to Network and Generator
+        mf.add_edge(variable_maker::FORK_FROM_SOURCE as usize, variable_maker::NETWORK as usize, total_flow, 0);
+        mf.add_edge(variable_maker::FORK_FROM_SOURCE as usize, variable_maker::GENERATOR as usize, total_flow, 0);
+
+        // Generator to System flow
         for t in 0..MINUTES_PER_DAY {
-            mf.add_edge(variable_maker::SOURCE as usize, variable_maker.get_system_flow_index(t).unwrap() as usize, 0, 0);
+
         }
 
         Self {
             mf:mf,
-            variable_map:variable_maker,
+            variable_map:variable_map,
+        }
+    }
+
+    fn add_action_variable_capacity(item_id: i32, mf: &mut MinCostFlow, variable_map: &mut VariableMaker, cap: i64) {
+        for t in 0..MINUTES_PER_DAY {
+            mf.add_edge(variable_map.get_action_variable_index(item_id, t, true).unwrap() as usize, variable_map.get_action_variable_index(item_id , t, false).unwrap() as usize, cap, 0);
+        }
+    }
+    fn add_battery_capacity(item_id: i32, mf: &mut MinCostFlow, variable_map: &mut VariableMaker, cap: i64) {
+        for t in 0..MINUTES_PER_DAY {
+            mf.add_edge(variable_map.get_battery_variable_index(item_id, t, true).unwrap() as usize, variable_map.get_battery_variable_index(item_id , t, false).unwrap() as usize, cap, 0);
         }
     }
 }
--- src/optimizer/variable_maker/mod.rs
+++ src/optimizer/variable_maker/mod.rs
@@ -15,9 +15,9 @@ pub const SOURCE: ItemId = 0;
 pub const SINK: ItemId = 1;
 pub const NETWORK: ItemId = 2;
 pub const GENERATOR: ItemId = 3;
+pub const FORK_FROM_SOURCE: ItemId = 4;
 
-pub const SYSTEM_FLOW: ItemId = -42;
-
+pub const WIRE: ItemId = -42;
 
 pub type PersistentVariableIndex = (ItemId, Timestamp);
 pub type PersistentVariableWithCapacityIndex = (ItemId, Timestamp, bool);
@@ -26,16 +26,20 @@ pub struct VariableMaker {
     variable_count: u32,
     
     persistent_variable_indices: BiMap<PersistentVariableIndex, u32>,
+    persistent_variable_with_capacity_index: BiMap<PersistentVariableWithCapacityIndex, u32>
 }
 impl VariableMaker {
     pub fn new(context: &OptimizerContext) -> Self {
-        let mut variable_count: u32 = 4;
+        let mut variable_count: u32 = (FORK_FROM_SOURCE + 1) as u32;
         let mut persistent: BiMap<PersistentVariableIndex, u32> = BiMap::new();
+        let mut persistent_with_capacity: BiMap<PersistentVariableWithCapacityIndex, u32> = BiMap::new();
         
         for var_action in context.get_variable_actions().iter() {
             let item_id = var_action.get_id() as ItemId;
             for t in 0..MINUTES_PER_DAY {
-                persistent.insert((item_id as ItemId, t as Timestamp), variable_count);
+                persistent_with_capacity.insert((item_id as ItemId, t as Timestamp, false), variable_count);
+                variable_count += 1;
+                persistent_with_capacity.insert((item_id as ItemId, t as Timestamp, true), variable_count);
                 variable_count += 1;
             }
         }
@@ -43,34 +47,34 @@ impl VariableMaker {
         for battery in context.get_batteries().iter() {
             let item_id = battery.get_id() as ItemId;
             for t in 0..MINUTES_PER_DAY {
-                persistent.insert((item_id as ItemId, t as Timestamp), variable_count);
+                persistent_with_capacity.insert((item_id as ItemId, t as Timestamp, false), variable_count);
+                variable_count += 1;
+                persistent_with_capacity.insert((item_id as ItemId, t as Timestamp, true), variable_count);
                 variable_count += 1;
             }
         }
 
         for t in 0..MINUTES_PER_DAY {
-            persistent.insert((SYSTEM_FLOW, t as Timestamp), variable_count);
+            persistent.insert((WIRE, t as Timestamp), variable_count);
             variable_count += 1;
         }
 
         Self {
             variable_count,
             persistent_variable_indices: persistent,
+            persistent_variable_with_capacity_index: persistent_with_capacity,
         }
     }
 
-    pub fn get_action_variable_index(&self, item_id: ItemId, timestamp: Timestamp) -> Option<u32> {
-        self.persistent_variable_indices.get_by_a(&(item_id, timestamp)).cloned()
+    pub fn get_action_variable_index(&self, item_id: ItemId, timestamp: Timestamp, incoming: bool) -> Option<u32> {
+        self.persistent_variable_with_capacity_index.get_by_a(&(item_id, timestamp, incoming)).cloned()
     }
-    pub fn get_constant_action_variable_index(&self, item_id: ItemId, timestamp: Timestamp) -> Option<u32> {
-        self.persistent_variable_indices.get_by_a(&(item_id, timestamp)).cloned()
-    }
-    pub fn get_battery_variable_index(&self, item_id: ItemId, timestamp: Timestamp) -> Option<u32> {
-        self.persistent_variable_indices.get_by_a(&(item_id, timestamp)).cloned()
+    pub fn get_battery_variable_index(&self, item_id: ItemId, timestamp: Timestamp, incoming: bool) -> Option<u32> {
+        self.persistent_variable_with_capacity_index.get_by_a(&(item_id, timestamp, incoming)).cloned()
     }
 
-    pub fn get_system_flow_index(&self, timestamp: Timestamp) -> Option<u32> {
-        self.persistent_variable_indices.get_by_a(&(SYSTEM_FLOW, timestamp)).cloned()
+    pub fn get_WIRE_index(&self, timestamp: Timestamp) -> Option<u32> {
+        self.persistent_variable_indices.get_by_a(&(WIRE, timestamp)).cloned()
     }
 
     pub fn get_variable_count(&self) -> u32 {
